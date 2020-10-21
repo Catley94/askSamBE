f70c17d21bcd54df6133d36b6d7440aa2d6786f6
positive-season <scp94373@gmail.com>
2020-10-21 11:32:13 +0100

    Added schema and delete route
    Added schema and delete route. Schema for data structure and clear database as a dev tool.
    
    Have also added a way to create in database, hence the clear database.

--- server.js
+++ server.js
@@ -5,10 +5,22 @@ const router = express.Router();
 const mongoose = require("mongoose");
 const questions = require("./model");
 const uri = "mongodb://localhost:27017/asksamdb";
+let sessionCount = 0;
 
 mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true });
 
 const connection = mongoose.connection;
+
+const questionSchema = new mongoose.Schema({
+  sessionID: Number,
+  questionID: Number,
+  question: String,
+  answered: Boolean,
+  answer: String
+})
+
+const Question = mongoose.model('Question', questionSchema);
+
 
 // const data = [
 //     {
@@ -34,23 +46,46 @@ connection.once("open", function() {
 
 app.use("/", router);
 
+
 router.route("/").get(function(req, res) {
   console.log("GET: \"/\"")
+  sessionCount++;
 })
 
 router.route("/submitquestion").post(function(req, res) {
   console.log("POST received from: ", req.url)
   res.send("Post Received")
-  let data = [
-    {
-      sessionID: 0,
-      questionID: 0,
-      question: req.query.question,
-      answered: false,
-      answer: ''
+  Question.count({}, function(response) {
+    console.log("Document count: ", response)
+    // console.log(response)
+  })
+  const question = new Question({
+    sessionID: sessionCount,
+    questionID: 0,
+    question: req.query.question,
+    answered: false,
+    answer: ''
+  })
+  question.save()
+  .then(function(response) {
+    console.log("saved")
+    console.log(response);
+  })
+  .catch(function(err) {
+    console.log("error");
+    console.log(err);
+  })
+  // Question.create({
+  //   sessionID: sessionCount,
+  //   questionID: 0,
+  //   question: req.query.question,
+  //   answered: false,
+  //   answer: ''
+  // }, function(response) {
+  //   console.log("success!");
+  //   console.log(response);
+  // })
 
-    }
-  ]
   console.log(req.query.question)
     // questions.insertOne(data, function(err, result) {
     //     if (err) {
@@ -60,6 +95,20 @@ router.route("/submitquestion").post(function(req, res) {
     //     }
     //   });
 });
+
+app.delete('/cleardatabase', function(req, res) {
+  console.log("Delete request received")
+  Question.deleteMany({
+    //parameters
+  }, function() {
+    console.log('Deleted all in database')
+    console.log("Database contents")
+    Question.find({}, function(response) {
+      console.log("Database contents: ")
+      console.log(response)
+    })
+  })
+}) 
 
 // router.route("/fetchdata").get(function(req, res) {
 //     employees.find({}, function(err, result) {
